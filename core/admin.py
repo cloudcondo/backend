1d7df5de791d21656aeccc93e4b69cc1da7cee4c
cloudcondo <thanu@cloudcrowdlabs.com>
2025-09-16 13:35:51 +0000

    Fix CI: separate workflow/.env/requirements, set buildx driver, clean admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,22 +1,28 @@
 from django.contrib import admin
 from .models import Condo, Unit, ParkingSpot, UnitParkingAssignment, ShortTermBooking
 
+
 @admin.register(Condo)
 class CondoAdmin(admin.ModelAdmin):
     list_display = ("name", "city", "province", "code", "created_at")
     search_fields = ("name", "city", "code")
+
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ("unit_number", "condo", "owner_name", "owner_email", "status", "created_at")
     list_filter = ("condo", "status")
     search_fields = ("unit_number", "owner_name", "owner_email")
+    autocomplete_fields = ("condo",)
+
 
 @admin.register(ParkingSpot)
 class ParkingSpotAdmin(admin.ModelAdmin):
     list_display = ("code", "condo", "level", "spot_type", "created_at")
     list_filter = ("condo", "spot_type", "level")
     search_fields = ("code",)
+    autocomplete_fields = ("condo",)
+
 
 @admin.register(UnitParkingAssignment)
 class UnitParkingAssignmentAdmin(admin.ModelAdmin):
@@ -25,10 +31,10 @@ class UnitParkingAssignmentAdmin(admin.ModelAdmin):
     search_fields = ("unit__unit_number", "parking_spot__code")
     autocomplete_fields = ("unit", "parking_spot")
 
+
 @admin.register(ShortTermBooking)
 class ShortTermBookingAdmin(admin.ModelAdmin):
     list_display = ("unit", "guest_last_name", "check_in", "check_out", "status", "parking_spot", "created_at")
     list_filter = ("unit__condo", "status", "id_type")
     search_fields = ("guest_first_name", "guest_last_name", "id_number", "vehicle_plate", "unit__unit_number")
     autocomplete_fields = ("unit", "parking_spot")
-python manage.py runserver 0.0.0.0:8001
